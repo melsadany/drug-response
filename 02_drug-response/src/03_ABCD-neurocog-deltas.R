5d0b4bf60f0e6b93e1403ef5c6d78646a77581d0
Muhammad Elsadany <muhammad-elsadany@uiowa.edu>
2023-08-16 13:24:39 -0500

    added the flanker figs with 24 hr window of meds in mind

--- 02_drug-response/src/03_ABCD-neurocog-deltas.R
+++ 02_drug-response/src/03_ABCD-neurocog-deltas.R
@@ -38,8 +38,12 @@ adhd.meds <- data.frame(drug = c("methylphenidate",
 missing.samples <- read_csv("/Dedicated/jmichaelson-wdata/msmuhammad/data/ABCD/meds/abcd5/subjects-missing-med-name.csv") %>%
   mutate(drop = T)
 abcd.meds.r <- read_rds("/Dedicated/jmichaelson-wdata/msmuhammad/data/ABCD/meds/abcd5/abcd5-meds-matrix.rds") %>%
+# abcd.meds.r <- read_rds("/Dedicated/jmichaelson-wdata/msmuhammad/data/ABCD/meds/abcd5/abcd5-meds-matrix-last24.rds") %>%
   as.data.frame() %>%
   select(c(1:2), any_of(all.adhd.meds$drug)) %>%
+  # pivot_longer(cols = c(3:ncol(.)), names_to = "med", values_to = "val24") %>%
+  # mutate(val24long = ifelse(val24 == 1, 1, 0)) %>% filter()
+  # mutate_at(.vars = c(3:ncol(.)), .funs = function(x) ifelse(x == 2, 1, 0)) %>% #just for the 24-hr meds data
   # filter(!(grepl("3", eventname) | grepl("4", eventname))) %>%
   mutate(methylphenidate = ifelse((methylphenidate+ritalin)>=1 & concerta == 0,1,0),
          amphetamine = ifelse((adderall+amphetamine)>=1,1,0),
@@ -84,7 +88,28 @@ abcd.nihtbx.filt <- inner_join(inner_join(demo, abcd.nihtbx.filt), abcd.meds) %>
   drop_na()
 ####
 # nihtbx performance based on PGS and categorized by drug -----------------
-pgs.nihtbx.drug <- inner_join(abcd.nihtbx.filt, abcd.pgs)
+abcd.meds.neither24 <- read_rds("/Dedicated/jmichaelson-wdata/msmuhammad/data/ABCD/meds/abcd5/abcd5-meds-matrix.rds") %>%
+  as.data.frame() %>%
+  select(c(1:2), any_of(all.adhd.meds$drug)) %>%
+  # filter(!(grepl("3", eventname) | grepl("4", eventname))) %>%
+  mutate(methylphenidate = ifelse((methylphenidate+ritalin)>=1 & concerta == 0,1,0),
+         amphetamine = ifelse((adderall+amphetamine)>=1,1,0),
+         lisdexamfetamine = ifelse((vyvanse+lisdexamfetamine)>=1,1,0),
+         stim = ifelse((methylphenidate+adderall+amphetamine+concerta+ritalin+vyvanse+lisdexamfetamine)>=1,1,0),
+         non_stim = ifelse((intuniv+strattera+tenex+atomoxetine+clonidine+guanfacine)>=1,1,0),
+         guanfacine = ifelse((guanfacine+tenex)>=1 & intuniv == 0,1,0),
+         atomoxetine = ifelse((atomoxetine+strattera)>=1,1,0),
+         neither = ifelse((stim+non_stim)>=1, 0,1)) %>% 
+  select(-c(ritalin, adderall, tenex, strattera,intuniv, vyvanse))
+abcd.meds.neither24 <- left_join(abcd.meds.neither24, missing.samples) %>%
+  filter(is.na(drop)) %>%
+  select(-drop) %>%
+  select(IID, eventname, neither)
+
+nei <- left_join(abcd.nihtbx.filt %>% select(-neither), abcd.meds.neither24) %>% filter(neither == 1)
+sample <- sample(nrow(nei), 600)
+sns <- abcd.nihtbx.filt %>% filter(stim == 1 | non_stim == 1)
+pgs.nihtbx.drug <- inner_join(rbind(nei[sample,],sns), abcd.pgs)
 pgs.nihtbx.drug %>% 
   mutate(adq = as.factor(paste0("ADHD_q", ntile(`ADHD-Demontis`, 4)))) %>%
   mutate(cogq = as.factor(paste0("gFactor_q", ntile(cog_gFactor, 4)))) %>%
@@ -99,16 +124,20 @@ pgs.nihtbx.drug %>%
   filter(drug %in% c("stim", "non_stim", "neither")) %>%
   # filter(drug %in% c("neither", "methylphenidate", "guanfacine")) %>%
   filter(grepl("ADHD", pgs) | grepl("gFa", pgs)) %>%
+  filter(grepl("flanker", task)) %>%
   mutate(task =sub("_agecorrected", "", task)) %>% mutate(task =sub("nihtbx_", "", task)) %>%
   # filter(pg_score>0) %>%
+  # filter(abs(pg_score)<3) %>%
   # ggplot(aes(x=pg_score, y=task_score, color = drug, group = interaction(q,drug))) +
   ggplot(aes(x=pg_score, y=task_score, color = drug)) +
-  # geom_point(size=0.1)+
+  geom_point(size=0.1)+
   geom_smooth(method = "gam", se = T, na.rm = T) +
   stat_cor(show.legend = F, size = 3, na.rm = T, method = "spearman")+
-  facet_grid2(rows = vars(task), cols = vars(pgs)) +
-  # facet_grid2(rows = vars(task), cols = vars(q)) +
-  labs(x="PGS", caption = paste0("n(samples):\n",
+  facet_grid2(cols = vars(pgs)) +
+  # facet_grid2(rows = vars(task), cols = vars(pgs)) +
+  labs(x="PGS", 
+       y ="flanker test score",
+       caption = paste0("n(samples):\n",
                                  "\tstim: ", sum(pgs.nihtbx.drug$stim), "\n",
                                  "\tnon_stim: ", sum(pgs.nihtbx.drug$non_stim), "\n",
                                  "\tneither: ", sum(pgs.nihtbx.drug$neither)))
